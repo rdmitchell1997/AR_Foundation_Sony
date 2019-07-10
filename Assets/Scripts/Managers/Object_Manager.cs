a71895cde0e66f8d2bbee7c3cbf63a994a376c87
rdmitchell1997 <up767724@myport.ac.uk>
2019-07-10 19:54:48 +0100

    Built Object and UI managers to allow for interaction
    Created a simple slider to allow the AR object to be rotated (this is being controlled by the AR Object itself in te update function)
    
    I also added a video texture to a quad which can be activated through a simple button and hidden in a simliar way, a future expansion would be to have the quad always facing the camera.
    
    next up is to create the information points on the model and add some of the data.

---
+++ Assets/Scripts/Managers/Object_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Object_Manager : singleton<Object_Manager>
+{
+    public GameObject model;
+    public Slider rotationSlider;
+
+    void Update()
+    {
+        model.transform.rotation = Quaternion.Euler (0f, rotationSlider.value, 0f);
+    }
+}
---
+++ Assets/Scripts/Managers/UI_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : singleton<UI_Manager>
+{
+
+    public GameObject videoScreen;
+
+    public void ShowVideo()
+    {
+        videoScreen.SetActive(true);
+    }
+}
