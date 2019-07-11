c1f93bb88d5d6c2d9db3e78c053812d28525af75
rdmitchell1997 <up767724@myport.ac.uk>
2019-07-11 09:17:28 +0100

    Minor Bug Fixes
    - Added reset button to reset the scene.
    
    - Added a lookat script to top feature point so it is always interactable.
    
    -Added GameManager to handle all scene related tasks

---
+++ Assets/Button_Look_At.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Look_At : MonoBehaviour
+{
+    private GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (camera == null)
+        {
+            camera = GameObject.FindWithTag("MainCamera");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.LookAt(camera.transform); 
+    }
+}
---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Manager : singleton<Game_Manager>
+{
+    public void RestartScene()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
